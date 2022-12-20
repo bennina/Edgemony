1754292f424beaa306cff05608ca56982bfab068
Elisabetta <elisabetta.monaco@publicissapient.com>
2022-12-20 14:59:02 +0100

    esercizio all for

--- 20-12-22/script.js
+++ 20-12-22/script.js
@@ -1,3 +1,20 @@
-let names = [];
+// FOR CLASSICO 
+let arrayCasuale = []; 
+console.log('FOR CLASSICO');
 
-console.log(names);
+for (let i = 1; i <= 10; i++) {
+    arrayCasuale.push(prompt("Scrivi qualcosa che ti viene in mente: parola" + i))
+}
+console.log(arrayCasuale);
+
+// FOR IN
+console.log('FOR IN');
+for (let index in arrayCasuale) {
+   console.log('elemento '+ index + ': ' + arrayCasuale[index]);
+}
+
+// FOR OF 
+console.log('FOR OF');
+for (var parole of arrayCasuale) {
+    console.log('parola: ' + parole);
+}
