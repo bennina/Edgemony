9e1a39a03476d09d13a8fa1eac880ee4cf5dc4cb
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-02-02 17:19:25 +0100

    esercizio

--- 2-2-23/esercizio/index.js
+++ 2-2-23/esercizio/index.js
@@ -1,8 +1,23 @@
 const http = require('http');
 const libri = require('./libri')
+const fs = require('fs')
+const about = fs.readFileSync('about.html');
+const home = fs.readFileSync('home.html');
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(libri))
+    
+    
+    if (req.url === '/')
+        res.end(home)
+    else if(req.url === '/chi-siamo')
+        res.end(about)
+    else if (req.url === '/api')
+        res.writeHead(200, { 'Content-Type': 'application/json' }),
+        res.end(JSON.stringify(libri))
+    else {
+        res.writeHead(404)
+        res.end('sorry! Not found')
+    }
 });
 
 
