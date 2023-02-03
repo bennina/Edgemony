19cff0a85695b6f55415e030d6f306fa09fd0a4d
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-02-03 17:18:38 +0100

    esercizio

--- 3-2-23/index.js
+++ 3-2-23/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const PORT = 8000
-
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-    res.send('home')
+    res.sendFile(path.resolve(__dirname,'./public/home.html'))
 })
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./public/about.html'))
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./public/contact.html'))
+})
+app.get('*', (req, res) => {
+    res.send('404! Not found')
+})
+
 app.listen(PORT, () => {
     console.log('start app')
 })
