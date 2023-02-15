e398a3e0bb3d58d73faf69b3354565531d707f3e
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-02-15 15:36:51 +0100

    init collection e create db

--- 15-2-23/index.js
+++ 15-2-23/index.js
@@ -1,7 +1,6 @@
 const  MongoClient = require('mongodb').MongoClient;
-//let url = "mongodb+srv://db_user:elisabettamonaco@mycluster.azlqk.mongodb.net/school?retryWrites=true";
 
-let url = "mongodb+srv://elisabettamonaco:0rqU0e67fEer4Wee@cluster0.zhgxdjx.mongodb.net/test"
+let url = "mongodb+srv://elisabettamonaco:0rqU0e67fEer4Wee@cluster0.zhgxdjx.mongodb.net/school?retryWrites=true"
 
 MongoClient.connect(url).then((db) =>
     {
@@ -11,4 +10,26 @@ MongoClient.connect(url).then((db) =>
     })
     .catch((err) => {
         console.log(err);
+    });
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        let dbase = db.db("school");
+
+        dbase.createCollection("students", function(err, res) {
+            if (err) throw err;
+            console.log("Collection creata");
+            db.close();
+        });
+    });
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        let dbase = db.db("school");
+        
+        dbase.createCollection("courses", function(err, res) {
+            if (err) throw err;
+            console.log("Collection creata");
+            db.close();
+        });
     });
