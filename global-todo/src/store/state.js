86026c36bf7bbc05999416b201e19463ed7841fe
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-04-05 15:58:55 +0200

    New mock

--- global-todo/src/store/state.js
+++ global-todo/src/store/state.js
@@ -1,4 +1,32 @@
 export const initialState = {
-  tasksListData: [],
+  tasksListData: [
+    {
+      id: 1,
+      todo: "Lorem psum sit",
+      completed: true,
+      image: "https://robohash.org/PBZ.png?set=set4&size=150x150",
+      username: "Primo",
+    },
+    {
+      id: 2,
+      todo: "Lorem psum sit",
+      completed: false,
+      image: "https://robohash.org/PBZ.png?set=set4&size=150x150",
+      username: "SECONDO",
+    },
+    {
+      id: 3,
+      todo: "Lorem psum sit",
+      completed: true,
+      image: "https://robohash.org/PBZ.png?set=set4&size=150x150",
+      username: "TRE",
+    },
+  ],
   isModalVisibile: false,
+  tempTodo: {
+    username: "",
+    todo: "",
+    image: "",
+    id: null,
+  },
 };
